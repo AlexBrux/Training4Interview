40b88eef10e9d60906f05595004868cfede8488f
AlexBrux <alex.louis.brux@gmail.com>
2017-02-16 19:33:25 +0100

    fix mat

--- matrix.c
+++ matrix.c
@@ -4,22 +4,26 @@
 
 int main (int arcg, char** argv ){
 
-	
-		int mata[3][3];
-		int matc[3][3];
-		int ii, jj, sum;
-			for(ii =0 ; ii < 3 ; ii++)
-				for( jj =0 ; jj < 3 ; jj++)
+		int AR=3, AC=2, BR=3, BC=5;	
+		int mata[AR][AC];
+		int CR , CC;
+		CR = AR ;
+		CC = BC;
+
+		int matc[CR][CC];
+		int ii, jj, sum=0;
+			for(ii =0 ; ii < AR ; ii++)
+				for( jj =0 ; jj < AC ; jj++)
 					mata[ii][jj] = ii+jj;
 					
-		int matb[3][3];
-			for( ii =0 ; ii < 3 ; ii++)
-				for( jj =0 ; jj < 3 ; jj++)
+		int matb[BR][BC];
+			for( ii =0 ; ii < BR ; ii++)
+				for( jj =0 ; jj < BC ; jj++)
 					matb[ii][jj] = ii*jj;
 					
-		for( int cc = 0 ; cc < 3 ; cc++){	
-			for( ii =0 ; ii < 3 ; ii++){
-				for( jj =0 ; jj < 3 ; jj++){
+		for( int cc = 0 ; cc < AR ; cc++){	
+			for( ii =0 ; ii < BC ; ii++){
+				for( jj =0 ; jj < BR ; jj++){
 					sum = sum + mata[cc][jj] * matb[jj][ii];
 				}
 				matc[cc][ii] = sum ;
@@ -28,9 +32,9 @@ int main (int arcg, char** argv ){
 		}
 			
 					
-		for( ii =0 ; ii < 3 ; ii++){
+		for( ii =0 ; ii < AR ; ii++){
 			printf("{");
-			for( jj =0 ; jj < 3 ; jj++)
+			for( jj =0 ; jj < AC ; jj++)
 				printf("%d,",mata[ii][jj]);
 			
 			printf("}\n");
@@ -38,14 +42,22 @@ int main (int arcg, char** argv ){
 
 	printf("\n");
 	
-		for( ii =0 ; ii < 3 ; ii++){
+		for( ii =0 ; ii < BR ; ii++){
 			printf("{");
-			for( jj =0 ; jj < 3 ; jj++)
+			for( jj =0 ; jj < BC ; jj++)
+				printf("%d,",matb[ii][jj]);
+			
+			printf("}\n");
+		}
+		
+		for( ii =0 ; ii < CR ; ii++){
+			printf("{");
+			for( jj =0 ; jj < CC ; jj++)
 				printf("%d,",matc[ii][jj]);
 			
 			printf("}\n");
 		}
-	return 0;
+return 0;
 }
 
 
